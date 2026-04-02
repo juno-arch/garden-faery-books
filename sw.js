6c1b63249010ace2cd8643f52e396f4df771daa2
Garden Faery <gardenfaeryy@gmail.com>
2026-04-01 19:04:19 -0700

    Bump version to v5.4, SW cache to v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'garden-faery-books-v13';
+const CACHE_NAME = 'garden-faery-books-v14';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
